92b85feb730e0fef216d8d82c76d5f0d1803e6bf
mahsa_naderkhah <mahsa.naderkhah@devolon.fi>
2022-05-07 21:01:29 +0430

    add feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -17,5 +19,15 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    public function test_models_can_be_found()
+    {
+        $user = User::factory()->state([
+            'name' => 'Abigail Otwell',
+        ])->create();
+
+        $expecteddUser = User::find($user->id);
+        $this->assertEquals($expecteddUser->name, $user->name);
+    // Use model in tests...
     }
 }
